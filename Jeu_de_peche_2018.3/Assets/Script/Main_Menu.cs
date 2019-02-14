f320c6254747a78f8afe8b456d7f233d2dc116cc
Leticia <leticiaclerc@hotmail.fr>
2019-02-14 17:15:44 +0100

    scene menu et impoitation du monstre

---
+++ Jeu_de_peche_2018.3/Assets/Script/Main_Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Main_Scene");
+    }
+
+    public void Gallery()
+    {
+        SceneManager.LoadScene("Gallery_Scene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
