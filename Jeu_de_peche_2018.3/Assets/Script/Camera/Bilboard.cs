1de887f7290b2ee02df11f8889cadb1745bc2983
Leticia <leticiaclerc@hotmail.fr>
2019-03-07 16:44:08 +0100

    prefab plante qui regard cam

---
+++ Jeu_de_peche_2018.3/Assets/Script/Camera/Bilboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bilboard : MonoBehaviour
+{
+    private GameObject MainCam;
+
+    void Start()
+    {
+        MainCam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + MainCam.transform.rotation * Vector3.forward,
+           MainCam.transform.rotation * Vector3.up);
+    }
+}
