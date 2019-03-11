b66711dff5a5fd6d2367b85663fd69799e768a6a
sevkron <sevkron@gmail.com>
2019-03-11 20:00:17 +0100

    test Rig + fear manager en route ! DOTween installé

--- Jeu_de_peche_2018.3/Assets/Script/Fear_Manager.cs
+++ Jeu_de_peche_2018.3/Assets/Script/Fear_Manager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using DG.Tweening;
+using UnityEngine.Audio;
 
 public class Fear_Manager : MonoBehaviour
 {
@@ -11,6 +14,14 @@ public class Fear_Manager : MonoBehaviour
 
     public GameObject m_Lantern;
     private GameMaster gm;
+    private PostProcessVolume pPVolume;
+    Vignette vignette;
+    private bool vignetteIsActive;
+
+    public AudioMixer audioMixer;
+    private AudioMixerSnapshot defaultSnapshot;
+    private AudioMixerSnapshot fearSnapshot;
+
     private float fearModifier;
     private float currentLight;
     // Start is called before the first frame update
@@ -18,7 +29,9 @@ public class Fear_Manager : MonoBehaviour
     {
         //m_currentFearLevel = 0;
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
-        
+        defaultSnapshot = audioMixer.FindSnapshot("Snapshot");
+        fearSnapshot = audioMixer.FindSnapshot("Fear");
+        //pPVolume = gm.gameObject.GetComponent<PostProcessVolume>();
     }
 
     // Update is called once per frame
@@ -35,20 +48,53 @@ public class Fear_Manager : MonoBehaviour
         }
         else if(currentLight <= 10)
         {
-            fearModifier = fearModifier + 0.2f ;
+            fearModifier = 2f ;
         }
         else
         {
             fearModifier = 0;
         }
 
-        if(m_currentFearLevel >= maxFear)
+        if(m_currentFearLevel > 20)
         {
+            vignetteIsActive = true;
+            vignette = ScriptableObject.CreateInstance<Vignette>();
+            vignette.enabled.Override(true);
+            vignette.intensity.Override(0.40f);
+
+            var volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, vignette);
+            volume.weight = 0f;
+
+            DOTween.Sequence()
+                .Append(DOTween.To(() => volume.weight, x => volume.weight = x, 1f, 2000f));
+        }
+        else if (m_currentFearLevel > 50)
+        {
+            fearSnapshot.TransitionTo(1f);
+        }
+        else if (m_currentFearLevel > 75)
+        {
+
+        }
+        else if(m_currentFearLevel >= maxFear)
+        {
+            defaultSnapshot.TransitionTo(.5f);
             //GetComponentInParent
             Debug.Log("You Dead");
-
             this.transform.position = gm.lastCheckPointPos;
             m_currentFearLevel = minFear;
+        }
+        else
+        {
+            if (vignetteIsActive == true)
+            {
+                vignette.enabled.Override(false);
+                vignette.intensity.Override(0);
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
