d69682d82acada8eb19ecbeb9e4d950345a37c6e
sevkron <sevkron@gmail.com>
2019-03-12 22:12:03 +0100

    Sound menu options + gameover

---
+++ Jeu_de_peche_2018.3/Assets/Script/Pause_Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEditor.Rendering.PostProcessing;
+using UnityEngine.UI;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public GameObject m_optionsPanel;
+
+    public AudioMixer m_settingsAudioMixer;
+
+    private float sliderVolume = 1f;
+
+    public void Options()
+    {
+        m_optionsPanel.SetActive(true);
+    }
+
+    public void MasterVolume(float vol)
+    {
+        sliderVolume = vol;
+    }
+
+    public void AcceptOptions()
+    {
+        m_settingsAudioMixer.SetFloat("Master", sliderVolume);
+    }
+
+    public void BackToPause()
+    {
+        m_optionsPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
