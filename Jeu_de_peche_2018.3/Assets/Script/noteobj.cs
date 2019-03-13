d4b6fe621f916a8283bd634065ce5cd858b05c5f
Leticia <leticiaclerc@hotmail.fr>
2019-03-13 08:56:01 +0100

    journal fini

--- Jeu_de_peche_2018.3/Assets/Script/noteobj.cs
+++ Jeu_de_peche_2018.3/Assets/Script/noteobj.cs
@@ -6,18 +6,23 @@ public class noteobj : MonoBehaviour
 {
     private GameObject Book;
     private GameObject DiaryButton;
+    private GameObject Player;
     public int m_numberinArray;
+    public GameObject m_Diary;
 
     private void Start()
     {
         Book = GameObject.FindGameObjectWithTag("CanvasBook");
         DiaryButton = GameObject.FindGameObjectWithTag("Diary");
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player" && Input.GetButton("Interact"))
         {
+            m_Diary.SetActive(true);
+            Player.GetComponent<CharacterController>().enabled = false;
 
             Book.GetComponent<Journal_test_Leo>().t_notes[m_numberinArray].SetActive(true);
             Book.GetComponent<Journal_test_Leo>().t_notes[m_numberinArray].transform.parent = Book.GetComponent<Journal_test_Leo>().children[Book.GetComponent<Journal_test_Leo>().index].transform;
