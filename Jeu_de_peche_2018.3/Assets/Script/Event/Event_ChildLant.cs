f42ceb45d70bb9f35738ec875a0d6b19da1ecdcf
Leticia <leticiaclerc@hotmail.fr>
2019-03-17 13:10:33 +0100

    pick up lamp anim pick up ajouter

--- Jeu_de_peche_2018.3/Assets/Script/Event/Event_ChildLant.cs
+++ Jeu_de_peche_2018.3/Assets/Script/Event/Event_ChildLant.cs
@@ -6,6 +6,7 @@ public class Event_ChildLant : MonoBehaviour
 {
     private GameObject Player;
     private GameObject Lantern;
+    private bool LanternGround;
 
     void Start()
     {
@@ -14,13 +15,28 @@ public class Event_ChildLant : MonoBehaviour
         Lantern.SetActive(false);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        LanternGround = true;
+    }
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" && Input.GetButton("Interact"))
+        if (other.tag == "Player" && Input.GetButton("Interact") && LanternGround == true)
         {
-            gameObject.SetActive(false);
-            Lantern.SetActive(true);
-            Player.GetComponent<Animator>().SetBool("Has Lantern", true);
+            LanternGround = false;
+            StartCoroutine(WaitAnimEnd());
         }
+    }
+
+    IEnumerator WaitAnimEnd()
+    {
+        Player.GetComponent<Animator>().SetTrigger("Pickup");
+        Player.GetComponent<CharacterController>().enabled = false;
+        yield return new WaitForSecondsRealtime(2.7f);
+        Player.GetComponent<CharacterController>().enabled = true;
+        gameObject.SetActive(false);
+        Lantern.SetActive(true);
+        Player.GetComponent<Animator>().SetBool("Has Lantern", true);
     }
 }
