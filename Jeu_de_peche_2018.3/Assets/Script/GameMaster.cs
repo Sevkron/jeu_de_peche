9ffeb05788d66af8d85ecff8282a3fd935240204
sevkron <sevkron@gmail.com>
2019-03-17 13:42:09 +0100

    canvas anim

--- Jeu_de_peche_2018.3/Assets/Script/GameMaster.cs
+++ Jeu_de_peche_2018.3/Assets/Script/GameMaster.cs
@@ -9,6 +9,7 @@ public class GameMaster : MonoBehaviour
     private GameObject Player;
     public AudioClip deathSFX;
     private Animator playerAnim;
+    public GameObject m_DeathScreen;
 
     private void Awake()
     {
@@ -27,6 +28,7 @@ public class GameMaster : MonoBehaviour
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         playerAnim = Player.GetComponent<Animator>();
+        m_DeathScreen = GameObject.FindGameObjectWithTag("CanvasBook").gameObject.transform.Find("DeathScreen").gameObject;
     }
 
     public void Death()
@@ -35,11 +37,13 @@ public class GameMaster : MonoBehaviour
         Player.GetComponent<CharacterController>().enabled = false;
         GetComponent<AudioSource>().PlayOneShot(deathSFX);
         Player.GetComponent<Fear_Manager>().m_currentFearLevel = 0;
+        m_DeathScreen.SetActive(true);
         //particule de smoke 
     }
 
     public void Respawn()
     {
+        m_DeathScreen.SetActive(false);
         Player.transform.position = lastCheckPointPos;
         playerAnim.SetTrigger("Respawn");
         Player.GetComponent<CharacterController>().enabled = true;
