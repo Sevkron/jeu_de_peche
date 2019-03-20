7befa9d77d3a1417f8323ab3b455be2672f5f3bc
Mauser96 <alexispro.sonnier@orange.fr>
2019-03-20 23:45:43 +0100

    menu suite

---
+++ Jeu_de_peche_2018.3/Assets/Script/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void PlayButton(string Level1)
+    {
+        SceneManager.LoadScene(Level1);
+    }
+
+   
+}
